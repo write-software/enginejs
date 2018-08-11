40fd5ff4a1bf6d1564fbf63de18154f8d952b8d0
steve <steve@trakware-solutions.co.uk>
2018-08-11 16:49:02 +0100

    2018-08-11.2

--- engine/common.js
+++ engine/common.js
@@ -1,4 +1,22 @@
-
+/****************************************************************
+ *
+ * The author of this software is Steve Egginton.
+ *
+ * Copyright (c) 2018 The Write Software Company Limited.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose without fee is hereby granted, provided that this entire notice
+ * is included in all copies of any software which is or includes a copy
+ * or modification of this software and in all copies of the supporting
+ * documentation for such software.
+ *
+ * THIS SOFTWARE IS BEING PROVIDED "AS IS", WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTY.  IN PARTICULAR, NEITHER THE AUTHOR NOR WRITE SOFTWARE MAKES ANY
+ * REPRESENTATION OR WARRANTY OF ANY KIND CONCERNING THE MERCHANTABILITY
+ * OF THIS SOFTWARE OR ITS FITNESS FOR ANY PARTICULAR PURPOSE.
+ *
+ ***************************************************************/
+ 
 // This is a set of useful command some of which are direct PHP copys
 
 function validMac(testStr) 
