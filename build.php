78769d3e1418ad93325aeb32d88dac515a73b3c7
steve <steve@trakware-solutions.co.uk>
2019-01-01 00:33:46 +0000

    2019-01-01
    Updated production.php to store and recovery production versions of .js files

--- build.php
+++ build.php
@@ -2,7 +2,7 @@
 
 function trace($msg)
 {
-    echo $msg . "<br>";
+    echo "<div>$msg</div>";
     ob_flush();
     flush();
 }
@@ -68,14 +68,14 @@ function recurse_package($entries)
         }
         else if (is_object ($entry) && substr($key,0,1) != "!")
         {
-            trace("[$key] $entry");
+            trace("[$key]");
             recurse_package($entry);
         }
     }    
 }
 
-trace("Start...");
-$package = file_get_contents("package.json");
+trace("<b>Start...</b>");
+$package = file_get_contents("./app/package.json");
 $package = json_decode($package);
 
 recursiveDelete(__DIR__ . "/distribution/");
@@ -90,7 +90,9 @@ trace("copying base files");
 @mkdir (__DIR__ . "/distribution/engine/vendors");
 copy(__DIR__ . '/index.html',__DIR__ . '/distribution/index.html'); 
 copy(__DIR__ . '/index.js',__DIR__ . '/distribution/index.js'); 
-copy(__DIR__ . '/package.json',__DIR__ . '/distribution/package.json'); 
+copy(__DIR__ . '/production.php',__DIR__ . '/distribution/production.php'); 
+copy(__DIR__ . '/build.php',__DIR__ . '/build/production.php'); 
+copy(__DIR__ . '/app/package.json',__DIR__ . '/distribution/app/package.json'); 
 trace("copying app files");
 recurse_copy("./app/","./distribution/app/");
 recurse_copy("./assets/","./distribution/assets/");
@@ -106,7 +108,7 @@ foreach ($vendors as $entry)
     trace("..." . $entry);
     recurse_copy("./engine/vendors/$entry","./distribution/engine/vendors/$entry");
 }
-trace("...End");
+trace("<b>...End</b>");
 
 
 ?>
--- production.php
+++ production.php
@@ -7,11 +7,30 @@ $js = $_GET['js'];
 if (stripos($js,".js") === false)
     $js .= ".js";
 
-$path = pathinfo($js);    
+$jsfn = str_replace("./","_",$js);
+$jsfn = str_replace("/","_",$jsfn);
 
-$script= file_get_contents($js);
-$myPacker = new Packer($script, 'Normal', true, false, false);
-$packed = $myPacker->pack();
-$packed .= PHP_EOL . "//# sourceURL=" . $path['basename'];
+if (!file_exists(__DIR__ . "/production/") )
+{
+    @mkdir(__DIR__ . "/production/");
+}
+$packed = "";
+if (file_exists(__DIR__ . "/production/" . $jsfn))
+{
+    $packed = file_get_contents(__DIR__ . "/production/" . $jsfn );
+}
+else
+{
+    $path = pathinfo($js);    
+    
+    $script= file_get_contents($js);
+    if ($script !== false)
+    {
+        $myPacker = new Packer($script, 'Normal', true, false, false);
+        $packed = $myPacker->pack();
+        $packed .= PHP_EOL . "//# sourceURL=" . $path['basename'];
+        file_put_contents(__DIR__ . "/production/" . $jsfn, $packed );
+    }
+}
 echo $packed;
 ?>
